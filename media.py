011bc82157a0325b06c8068ef202018f71e9835a
Dhruv Parthasarathy <dhruv@udacity.com>
2015-05-31 16:11:22 -0700

    fix indentation

--- media.py
+++ media.py
@@ -9,8 +9,8 @@ class Movie:
     """
 
     def __init__(self, title, trailer_youtube_url, poster_image_url, description):
-    """Instantiates a movie object.
-    """
+        """Instantiates a movie object.
+        """
         self.title = title
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
